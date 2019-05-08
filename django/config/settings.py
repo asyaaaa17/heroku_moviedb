f941904069b4f1e9a5507fc4b97a217c740cd911
asyaaaa17 <asyaaaa17@gmail.com>
2019-05-08 16:12:49 +0300

    updated static

--- django/config/settings.py
+++ django/config/settings.py
@@ -134,13 +134,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'gathered_static_files')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
 
-]
 
 LOGIN_REDIRECT_URL = 'core:MovieList'
 
